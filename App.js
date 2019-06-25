85ef424d5da8808efac2bb47fbc582de49c3a102
Ashutosh Bharwaj <47935938+bhardwajashutosh9520@users.noreply.github.com>
2019-06-25 18:59:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './component/greet'
+import Form from './component/form'
+const a = 20;
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+      <Form />
+      </div>
+      );
+  }
+}
+
+export default App;
